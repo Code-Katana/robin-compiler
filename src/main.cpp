57f7bdfcc5d3106a4608ddc4d134ee5923211fab
samirahmad5445 <samirahmad5445@gmail.com>
2024-11-08 14:03:59 +0200

    stringify the remaining nodes

--- src/main.cpp
+++ src/main.cpp
@@ -9,11 +9,21 @@ using namespace std;
 
 int main()
 {
+  Literal *a = new StringLiteral("Hello World");
+  Literal *b = new IntegerLiteral(69);
+  Literal *c = new FloatLiteral(3.14);
+  Literal *d = new BooleanLiteral(true);
+  Literal *e = new IntegerLiteral(88);
   Identifier *x = new Identifier("x");
   Identifier *y = new Identifier("y");
   Identifier *z = new Identifier("z");
 
   vector<Identifier *> vars = {x, y, z};
+
+  // Literal *arr = new ArrayLiteral(elements);
+
+  // AstNode *expr = new AdditiveExpression(
+  //     new PrimaryExpression(f), new PrimaryExpression(b), "+");
 
   AstNode *stmt = new ReadStatement(vars);
 

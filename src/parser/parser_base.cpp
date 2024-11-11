dbb7e837cc99f897ca3bb8fab6dd14bb59bf5f54
Zenhom-1 <mahmoud43wzaaa@gmail.com>
2024-11-11 15:57:49 +0200

    add `ParserBase` class

---
+++ src/parser/parser_base.cpp
@@ -0,0 +1,22 @@
+#include "parser_base.h"
+
+ParserBase::ParserBase(ScannerBase *scanner)
+{
+  sc = scanner;
+
+  current_token = sc->get_token();
+}
+
+Token ParserBase::match(TokenType type)
+{
+  Token placeholder = current_token;
+
+  if (current_token.type != type)
+  {
+    current_token = {"$", TokenType::END_OF_FILE};
+    return placeholder;
+  }
+
+  current_token = sc->get_token();
+  return placeholder;
+}

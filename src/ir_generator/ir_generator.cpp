c12a95b8ea2a280a73bd9d183806fa1927f25012
hakeem <medohakeem@gmail.com>
2025-03-28 14:54:56 +0200

    add `codegenProgram` method , `codegenFunction` method and `codegenstatement` method

--- src/ir_generator/ir_generator.cpp
+++ src/ir_generator/ir_generator.cpp
@@ -65,7 +65,121 @@ Value *IRGenerator::codegen(AstNode *node)
     return codegenStatement(stmt);
   return nullptr;
 }
+Value *IRGenerator::codegenProgram(ProgramDefinition *program)
+{
+  // Create main function
+  FunctionType *funcType = FunctionType::get(Type::getInt32Ty(context), false);
+  llvm::Function *mainFunc = llvm::Function::Create(funcType, llvm::Function::ExternalLinkage, program->program_name->name, module.get());
 
+  BasicBlock *entry = BasicBlock::Create(context, "entry", mainFunc);
+  builder.SetInsertPoint(entry);
+
+  // Process global variables
+  for (auto global : program->globals)
+  {
+    codegen(global->def);
+  }
+
+  // Process program body
+  for (auto stmt : program->body)
+  {
+    codegen(stmt);
+  }
+
+  builder.CreateRet(ConstantInt::get(context, APInt(32, 0)));
+  return mainFunc;
+}
+
+Value *IRGenerator::codegenFunction(FunctionDefinition *func)
+{
+  // Get return type
+  Type *retType = getLLVMType(Symbol::get_datatype(func->return_type->return_type),
+                              Symbol::get_dimension(func->return_type->return_type));
+
+  // Get parameter types
+  std::vector<Type *> paramTypes;
+  for (auto param : func->parameters)
+  {
+    if (auto decl = dynamic_cast<VariableDeclaration *>(param->def))
+    {
+      Type *ty = getLLVMType(Symbol::get_datatype(decl->datatype),
+                             Symbol::get_dimension(decl->datatype));
+      paramTypes.push_back(ty);
+    }
+  }
+
+  // Explicitly qualify LLVM components with llvm::
+  llvm::FunctionType *funcType = llvm::FunctionType::get(retType, paramTypes, false);
+  llvm::Function *llvmFunc = llvm::Function::Create( // <-- Fully qualified
+      funcType,
+      llvm::Function::ExternalLinkage,
+      func->funcname->name,
+      module.get());
+
+  // Create entry block
+  llvm::BasicBlock *entry = llvm::BasicBlock::Create(context, "entry", llvmFunc);
+  builder.SetInsertPoint(entry);
+  pushScope();
+
+  // Process parameters
+  unsigned idx = 0;
+  for (auto &arg : llvmFunc->args())
+  {
+    if (auto decl = dynamic_cast<VariableDeclaration *>(func->parameters[idx++]->def))
+    {
+      llvm::AllocaInst *alloca = builder.CreateAlloca(arg.getType());
+      builder.CreateStore(&arg, alloca);
+      symbolTable.top()[decl->variables[0]->name] = alloca;
+    }
+  }
+
+  // Process function body
+  for (auto stmt : func->body)
+  {
+    codegen(stmt);
+  }
+
+  if (retType->isVoidTy())
+  {
+    builder.CreateRetVoid();
+  }
+
+  popScope();
+  return llvmFunc;
+}
+
+Value *IRGenerator::codegenStatement(Statement *stmt)
+{
+  if (auto varDecl = dynamic_cast<VariableDeclaration *>(stmt))
+  {
+    return codegenVariableDeclaration(varDecl);
+  }
+  if (auto assign = dynamic_cast<AssignmentExpression *>(stmt))
+  {
+    return codegenAssignment(assign);
+  }
+  if (auto write = dynamic_cast<WriteStatement *>(stmt))
+  {
+    return codegenWriteStatement(write);
+  }
+  if (auto read = dynamic_cast<ReadStatement *>(stmt))
+  {
+    return codegenReadStatement(read);
+  }
+  if (auto ifStmt = dynamic_cast<IfStatement *>(stmt))
+  {
+    return codegenConditional(ifStmt);
+  }
+  if (auto loop = dynamic_cast<WhileLoop *>(stmt))
+  {
+    return codegenLoop(loop);
+  }
+  if (auto forLoop = dynamic_cast<ForLoop *>(stmt))
+  {
+    return codegenForLoop(forLoop);
+  }
+  return codegenExpression(dynamic_cast<Expression *>(stmt));
+}
 Value *IRGenerator::findValue(const string &name)
 {
   for (auto scope = symbolTable.top(); !symbolTable.empty(); symbolTable.pop())

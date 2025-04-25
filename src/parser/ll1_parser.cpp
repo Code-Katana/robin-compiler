87fa339a4c64f836a8d15614e2a271cb9e25db62
Zenhom-1 <mahmoud43wzaaa@gmail.com>
2025-04-25 19:58:12 +0300

    create `push_rule` method implement

--- src/parser/ll1_parser.cpp
+++ src/parser/ll1_parser.cpp
@@ -1,5 +1,7 @@
 #include "ll1_parser.h"
 
+#include <cstring>
+
 LL1Parser::LL1Parser(ScannerBase *sc) : ParserBase(sc)
 {
   fill_table();
@@ -22,6 +24,18 @@ AstNode *LL1Parser::parse_ast()
     else if (!top.isTerm)
     {
       int rule = parseTable[(int)top.nt][(int)current_token.type];
+      // if(rule == 73 && current_token.type == TokenType::ID_SY)
+      // {
+      //   get_token();
+      //   if(current_token.type == TokenType::EQUAL_OP)
+      //   {
+      //     rule = 72;
+      //   }
+      //   else
+      //   {
+
+      //   }
+      // }
       if (rule == 0)
       {
         syntax_error("Unexpected token in prediction");
@@ -687,4 +701,650 @@ void LL1Parser::fill_table()
 
   parseTable[(int)NonTerminal::Call_Expr_Tail_NT][(int)TokenType::RIGHT_PR] = 134;
   parseTable[(int)NonTerminal::Call_Expr_Tail_NT][(int)TokenType::COMMA_SY] = 135;
+}
+
+void LL1Parser::push_rule(int rule)
+{
+  switch (rule)
+  {
+  case 1:
+    st.push(Symbol(1));
+    st.push(Symbol(TokenType::END_OF_FILE));
+    st.push(Symbol(NonTerminal::Program_NT));
+    st.push(Symbol(NonTerminal::Function_List_NT));
+    break;
+  case 2:
+    st.push(Symbol(2));
+    break;
+  case 3:
+    st.push(Symbol(3));
+    st.push(Symbol(NonTerminal::Function_List_NT));
+    st.push(Symbol(NonTerminal::Function_NT));
+    break;
+  case 4:
+    st.push(Symbol(4));
+    st.push(Symbol(TokenType::FUNC_KW));
+    st.push(Symbol(TokenType::END_KW));
+    st.push(Symbol(NonTerminal::Command_Seq_NT));
+    st.push(Symbol(TokenType::BEGIN_KW));
+    st.push(Symbol(NonTerminal::Declaration_Seq_NT));
+    st.push(Symbol(TokenType::HAS_KW));
+    st.push(Symbol(TokenType::ID_SY));
+    st.push(Symbol(NonTerminal::Function_Type_NT));
+    st.push(Symbol(TokenType::FUNC_KW));
+    break;
+  case 5:
+    st.push(Symbol(5));
+    st.push(Symbol(NonTerminal::Type_NT));
+    break;
+  case 6:
+    st.push(Symbol(6));
+    st.push(Symbol(TokenType::VOID_TY));
+    break;
+  case 7:
+    st.push(Symbol(7));
+    st.push(Symbol(NonTerminal::Array_Type_NT));
+
+    break;
+  case 8:
+    st.push(Symbol(8));
+    st.push(Symbol(NonTerminal::Primitive_NT));
+    break;
+  case 9:
+    st.push(Symbol(9));
+    st.push(Symbol(NonTerminal::Array_Type_Tail_NT));
+    st.push(Symbol(TokenType::LEFT_SQUARE_PR));
+    break;
+  case 10:
+    st.push(Symbol(10));
+    st.push(Symbol(TokenType::RIGHT_SQUARE_PR));
+    st.push(Symbol(NonTerminal::Primitive_NT));
+    break;
+  case 11:
+    st.push(Symbol(11));
+    st.push(Symbol(TokenType::RIGHT_SQUARE_PR));
+    st.push(Symbol(NonTerminal::Array_Type_NT));
+    break;
+  case 12:
+    st.push(Symbol(12));
+    st.push(Symbol(TokenType::INTEGER_TY));
+    break;
+  case 13:
+    st.push(Symbol(13));
+    st.push(Symbol(TokenType::BOOLEAN_TY));
+    break;
+  case 14:
+    st.push(Symbol(14));
+    st.push(Symbol(TokenType::STRING_TY));
+    break;
+  case 15:
+    st.push(Symbol(15));
+    st.push(Symbol(TokenType::FLOAT_TY));
+    break;
+  case 16:
+    st.push(Symbol(16));
+    st.push(Symbol(NonTerminal::Block_NT));
+    st.push(Symbol(TokenType::IS_KW));
+    st.push(Symbol(TokenType::ID_SY));
+    st.push(Symbol(TokenType::PROGRAM_KW));
+    break;
+  case 17:
+    st.push(Symbol(17));
+    st.push(Symbol(TokenType::END_KW));
+    st.push(Symbol(NonTerminal::Command_Seq_NT));
+    st.push(Symbol(TokenType::BEGIN_KW));
+    st.push(Symbol(NonTerminal::Declaration_Seq_NT));
+    break;
+  case 18:
+    st.push(Symbol(18));
+    break;
+  case 19:
+    st.push(Symbol(19));
+    st.push(Symbol(NonTerminal::Declaration_Seq_NT));
+    st.push(Symbol(NonTerminal::Declaration_NT));
+    break;
+  case 20:
+    st.push(Symbol(20));
+    st.push(Symbol(NonTerminal::Decl_Tail_NT));
+    st.push(Symbol(TokenType::VAR_KW));
+    break;
+  case 21:
+    st.push(Symbol(21));
+    st.push(Symbol(NonTerminal::Decl_Tail_Right_NT));
+    st.push(Symbol(NonTerminal::Type_NT));
+    st.push(Symbol(TokenType::COLON_SY));
+    st.push(Symbol(NonTerminal::Variable_List_NT));
+    break;
+  case 22:
+    st.push(Symbol(22));
+    st.push(Symbol(TokenType::SEMI_COLON_SY));
+    break;
+  case 23:
+    st.push(Symbol(23));
+    st.push(Symbol(TokenType::SEMI_COLON_SY));
+    st.push(Symbol(NonTerminal::Decl_Init_NT));
+    st.push(Symbol(TokenType::EQUAL_OP));
+    break;
+  case 24:
+    st.push(Symbol(24));
+    st.push(Symbol(NonTerminal::Or_Expr_NT));
+    break;
+  case 25:
+    st.push(Symbol(25));
+    st.push(Symbol(TokenType::RIGHT_CURLY_PR));
+    st.push(Symbol(NonTerminal::Array_Value_NT));
+    st.push(Symbol(TokenType::LEFT_CURLY_PR));
+    break;
+  case 26:
+    st.push(Symbol(26));
+    break;
+  case 27:
+    st.push(Symbol(27));
+    st.push(Symbol(NonTerminal::Value_List_NT));
+    break;
+  case 28:
+    st.push(Symbol(28));
+    st.push(Symbol(NonTerminal::Array_Nested_Value_NT));
+    break;
+  case 29:
+    st.push(Symbol(29));
+    st.push(Symbol(NonTerminal::More_Nested_NT));
+    st.push(Symbol(TokenType::RIGHT_CURLY_PR));
+    st.push(Symbol(NonTerminal::Value_List_NT));
+    st.push(Symbol(TokenType::LEFT_CURLY_PR));
+    break;
+  case 30:
+    st.push(Symbol(30));
+    break;
+  case 31:
+    st.push(Symbol(31));
+    st.push(Symbol(NonTerminal::Array_Nested_Value_NT));
+    st.push(Symbol(TokenType::COMMA_SY));
+    break;
+  case 32:
+    st.push(Symbol(32));
+    st.push(Symbol(NonTerminal::More_Values_NT));
+    st.push(Symbol(NonTerminal::Or_Expr_NT));
+    break;
+  case 33:
+    st.push(Symbol(33));
+    break;
+  case 34:
+    st.push(Symbol(34));
+    st.push(Symbol(NonTerminal::Value_List_NT));
+    st.push(Symbol(TokenType::COMMA_SY));
+    break;
+  case 35:
+    st.push(Symbol(35));
+    st.push(Symbol(NonTerminal::More_Variables_NT));
+    st.push(Symbol(TokenType::ID_SY));
+
+    break;
+  case 36:
+    st.push(Symbol(36));
+    break;
+  case 37:
+    st.push(Symbol(37));
+    st.push(Symbol(NonTerminal::Variable_List_NT));
+    st.push(Symbol(TokenType::COMMA_SY));
+    break;
+  case 38:
+    st.push(Symbol(38));
+    st.push(Symbol(NonTerminal::Command_Seq_Tail_NT));
+    st.push(Symbol(NonTerminal::Command_NT));
+    break;
+  case 39:
+    st.push(Symbol(39));
+    break;
+  case 40:
+    st.push(Symbol(40));
+    st.push(Symbol(NonTerminal::Command_Seq_Tail_NT));
+    st.push(Symbol(NonTerminal::Command_NT));
+    break;
+  case 41:
+    st.push(Symbol(41));
+    break;
+  case 42:
+    st.push(Symbol(42));
+    st.push(Symbol(TokenType::SEMI_COLON_SY));
+    st.push(Symbol(TokenType::SKIP_KW));
+    break;
+  case 43:
+    st.push(Symbol(43));
+    st.push(Symbol(TokenType::SEMI_COLON_SY));
+    st.push(Symbol(TokenType::STOP_KW));
+    break;
+  case 44:
+    st.push(Symbol(44));
+    st.push(Symbol(NonTerminal::Read_Statement_NT));
+    break;
+  case 45:
+    st.push(Symbol(45));
+    st.push(Symbol(NonTerminal::Write_Statement_NT));
+    break;
+  case 46:
+    st.push(Symbol(46));
+    st.push(Symbol(NonTerminal::Loops_NT));
+    break;
+  case 47:
+    st.push(Symbol(47));
+    st.push(Symbol(NonTerminal::IF_Statement_NT));
+    break;
+  case 48:
+    st.push(Symbol(48));
+    st.push(Symbol(NonTerminal::Return_NT));
+    break;
+  case 49:
+    st.push(Symbol(49));
+    st.push(Symbol(NonTerminal::Expr_Statement_NT));
+    break;
+  case 50:
+    st.push(Symbol(50));
+    st.push(Symbol(NonTerminal::Declaration_NT));
+    break;
+  case 51:
+    st.push(Symbol(51));
+    st.push(Symbol(TokenType::SEMI_COLON_SY));
+    st.push(Symbol(NonTerminal::Variable_List_read_NT));
+    st.push(Symbol(TokenType::READ_KW));
+    break;
+  case 52:
+    st.push(Symbol(52));
+    st.push(Symbol(TokenType::SEMI_COLON_SY));
+    st.push(Symbol(NonTerminal::Expr_List_NT));
+    st.push(Symbol(TokenType::WRITE_KW));
+    break;
+  case 53:
+    st.push(Symbol(53));
+    st.push(Symbol(NonTerminal::More_Variables_read_NT));
+    st.push(Symbol(NonTerminal::Index_Chain_NT));
+    st.push(Symbol(TokenType::ID_SY));
+    break;
+  case 54:
+    st.push(Symbol(54));
+    break;
+  case 55:
+    st.push(Symbol(55));
+    st.push(Symbol(NonTerminal::Variable_List_read_NT));
+    st.push(Symbol(TokenType::COMMA_SY));
+    break;
+  case 56:
+    st.push(Symbol(56));
+    st.push(Symbol(NonTerminal::More_Exprs_NT));
+    st.push(Symbol(NonTerminal::Expr_NT));
+    break;
+  case 57:
+    st.push(Symbol(57));
+    break;
+  case 58:
+    st.push(Symbol(58));
+    st.push(Symbol(NonTerminal::Expr_List_NT));
+    st.push(Symbol(TokenType::COMMA_SY));
+    break;
+  case 59:
+    st.push(Symbol(59));
+    st.push(Symbol(NonTerminal::For_Loop_NT));
+    break;
+  case 60:
+    st.push(Symbol(60));
+    st.push(Symbol(NonTerminal::While_Loop_NT));
+    break;
+  case 61:
+    st.push(Symbol(61));
+    st.push(Symbol(TokenType::FOR_KW));
+    st.push(Symbol(TokenType::END_KW));
+    st.push(Symbol(NonTerminal::Command_Seq_NT));
+    st.push(Symbol(TokenType::DO_KW));
+    st.push(Symbol(NonTerminal::Expr_NT));
+    st.push(Symbol(TokenType::SEMI_COLON_SY));
+    st.push(Symbol(NonTerminal::Or_Expr_NT));
+    st.push(Symbol(TokenType::SEMI_COLON_SY));
+    st.push(Symbol(NonTerminal::Integer_Assign_NT));
+    st.push(Symbol(TokenType::FOR_KW));
+    break;
+  case 62:
+    st.push(Symbol(62));
+    st.push(Symbol(NonTerminal::Expr_NT));
+    st.push(Symbol(TokenType::EQUAL_OP));
+    st.push(Symbol(TokenType::ID_SY));
+    break;
+  case 63:
+    st.push(Symbol(63));
+    st.push(Symbol(TokenType::WHILE_KW));
+    st.push(Symbol(TokenType::END_KW));
+    st.push(Symbol(NonTerminal::Command_Seq_NT));
+    st.push(Symbol(TokenType::DO_KW));
+    st.push(Symbol(NonTerminal::Or_Expr_NT));
+    st.push(Symbol(TokenType::WHILE_KW));
+    break;
+  case 64:
+    st.push(Symbol(64));
+    st.push(Symbol(TokenType::IF_KW));
+    st.push(Symbol(TokenType::END_KW));
+    st.push(Symbol(NonTerminal::Else_Part_NT));
+    st.push(Symbol(NonTerminal::Command_Seq_NT));
+    st.push(Symbol(TokenType::THEN_KW));
+    st.push(Symbol(NonTerminal::Expr_NT));
+    st.push(Symbol(TokenType::IF_KW));
+    break;
+  case 65:
+    st.push(Symbol(65));
+    break;
+  case 66:
+    st.push(Symbol(66));
+    st.push(Symbol(NonTerminal::Command_Seq_NT));
+    st.push(Symbol(TokenType::ELSE_KW));
+    break;
+  case 67:
+    st.push(Symbol(67));
+    st.push(Symbol(TokenType::SEMI_COLON_SY));
+    st.push(Symbol(NonTerminal::Return_Value_NT));
+    st.push(Symbol(TokenType::RETURN_KW));
+    break;
+  case 68:
+    st.push(Symbol(68));
+    break;
+  case 69:
+    st.push(Symbol(69));
+    st.push(Symbol(NonTerminal::Or_Expr_NT));
+    break;
+  case 70:
+    st.push(Symbol(70));
+    st.push(Symbol(TokenType::SEMI_COLON_SY));
+    st.push(Symbol(NonTerminal::Expr_NT));
+    break;
+  case 71:
+    st.push(Symbol(71));
+    st.push(Symbol(NonTerminal::Assign_Expr_NT));
+    break;
+  case 72:
+    st.push(Symbol(72));
+    st.push(Symbol(NonTerminal::Assign_Expr_Value_NT));
+    st.push(Symbol(TokenType::EQUAL_OP));
+    st.push(Symbol(NonTerminal::Assignable_Expr_NT));
+    break;
+  case 73:
+    st.push(Symbol(73));
+    st.push(Symbol(NonTerminal::Or_Expr_NT));
+    break;
+  case 74:
+    st.push(Symbol(74));
+    st.push(Symbol(NonTerminal::Expr_NT));
+    break;
+  case 75:
+    st.push(Symbol(75));
+    st.push(Symbol(TokenType::RIGHT_CURLY_PR));
+    st.push(Symbol(NonTerminal::Array_Value_NT));
+    st.push(Symbol(TokenType::LEFT_CURLY_PR));
+    break;
+  case 76:
+    st.push(Symbol(76));
+    st.push(Symbol(NonTerminal::Index_Chain_NT));
+    st.push(Symbol(TokenType::ID_SY));
+    break;
+  case 77:
+    st.push(Symbol(77));
+    st.push(Symbol(NonTerminal::Or_Tail_NT));
+    st.push(Symbol(NonTerminal::And_Expr_NT));
+    break;
+  case 78:
+    st.push(Symbol(78));
+    break;
+  case 79:
+    st.push(Symbol(79));
+    st.push(Symbol(NonTerminal::Or_Tail_NT));
+    st.push(Symbol(NonTerminal::And_Expr_NT));
+    st.push(Symbol(TokenType::OR_KW));
+    break;
+  case 80:
+    st.push(Symbol(80));
+    st.push(Symbol(NonTerminal::And_Tail_NT));
+    st.push(Symbol(NonTerminal::Equality_Expr_NT));
+    break;
+  case 81:
+    st.push(Symbol(81));
+    break;
+  case 82:
+    st.push(Symbol(82));
+    st.push(Symbol(NonTerminal::And_Tail_NT));
+    st.push(Symbol(NonTerminal::Equality_Expr_NT));
+    st.push(Symbol(TokenType::AND_KW));
+    break;
+  case 83:
+    st.push(Symbol(83));
+    st.push(Symbol(NonTerminal::Equality_Tail_NT));
+    st.push(Symbol(NonTerminal::Relational_Expr_NT));
+    break;
+  case 84:
+    st.push(Symbol(84));
+    break;
+  case 85:
+    st.push(Symbol(85));
+    st.push(Symbol(NonTerminal::Equality_Tail_NT));
+    st.push(Symbol(NonTerminal::Relational_Expr_NT));
+    st.push(Symbol(NonTerminal::OP_Equal_NT));
+    break;
+  case 86:
+    st.push(Symbol(86));
+    st.push(Symbol(TokenType::IS_EQUAL_OP));
+    break;
+  case 87:
+    st.push(Symbol(87));
+    st.push(Symbol(TokenType::NOT_EQUAL_OP));
+    break;
+  case 88:
+    st.push(Symbol(88));
+    st.push(Symbol(NonTerminal::Relational_Tail_NT));
+    st.push(Symbol(NonTerminal::Additive_Expr_NT));
+    break;
+  case 89:
+    st.push(Symbol(89));
+    break;
+  case 90:
+    st.push(Symbol(90));
+    st.push(Symbol(NonTerminal::Relational_Tail_NT));
+    st.push(Symbol(NonTerminal::Additive_Expr_NT));
+    st.push(Symbol(NonTerminal::Relation_NT));
+    break;
+  case 91:
+    st.push(Symbol(91));
+    st.push(Symbol(TokenType::LESS_EQUAL_OP));
+    break;
+  case 92:
+    st.push(Symbol(92));
+    st.push(Symbol(TokenType::LESS_THAN_OP));
+    break;
+  case 93:
+    st.push(Symbol(93));
+    st.push(Symbol(TokenType::GREATER_THAN_OP));
+    break;
+  case 94:
+    st.push(Symbol(94));
+    st.push(Symbol(TokenType::GREATER_EQUAL_OP));
+    break;
+  case 95:
+    st.push(Symbol(95));
+    st.push(Symbol(NonTerminal::Additive_Tail_NT));
+    st.push(Symbol(NonTerminal::Multiplicative_Expr_NT));
+    break;
+  case 96:
+    st.push(Symbol(96));
+    break;
+  case 97:
+    st.push(Symbol(97));
+    st.push(Symbol(NonTerminal::Additive_Tail_NT));
+    st.push(Symbol(NonTerminal::Multiplicative_Expr_NT));
+    st.push(Symbol(NonTerminal::Weak_OP_NT));
+    break;
+  case 98:
+    st.push(Symbol(98));
+    st.push(Symbol(TokenType::PLUS_OP));
+    break;
+  case 99:
+    st.push(Symbol(99));
+    st.push(Symbol(TokenType::MINUS_OP));
+    break;
+  case 100:
+    st.push(Symbol(100));
+    st.push(Symbol(NonTerminal::Multiplicative_Tail_NT));
+    st.push(Symbol(NonTerminal::Unary_Expr_NT));
+    break;
+  case 101:
+    st.push(Symbol(101));
+    break;
+  case 102:
+    st.push(Symbol(102));
+    st.push(Symbol(NonTerminal::Multiplicative_Tail_NT));
+    st.push(Symbol(NonTerminal::Unary_Expr_NT));
+    st.push(Symbol(NonTerminal::Strong_OP_NT));
+    break;
+  case 103:
+    st.push(Symbol(103));
+    st.push(Symbol(TokenType::MULT_OP));
+    break;
+  case 104:
+    st.push(Symbol(104));
+    st.push(Symbol(TokenType::DIVIDE_OP));
+    break;
+  case 105:
+    st.push(Symbol(105));
+    st.push(Symbol(TokenType::MOD_OP));
+    break;
+  case 106:
+    st.push(Symbol(106));
+    st.push(Symbol(NonTerminal::Expr_NT));
+    st.push(Symbol(TokenType::MINUS_OP));
+    break;
+  case 107:
+    st.push(Symbol(107));
+    st.push(Symbol(NonTerminal::Expr_NT));
+    st.push(Symbol(TokenType::STRINGIFY_OP));
+    break;
+  case 108:
+    st.push(Symbol(108));
+    st.push(Symbol(NonTerminal::Expr_NT));
+    st.push(Symbol(TokenType::BOOLEAN_OP));
+    break;
+  case 109:
+    st.push(Symbol(109));
+    st.push(Symbol(NonTerminal::Expr_NT));
+    st.push(Symbol(TokenType::ROUND_OP));
+    break;
+  case 110:
+    st.push(Symbol(110));
+    st.push(Symbol(NonTerminal::Expr_NT));
+    st.push(Symbol(TokenType::LENGTH_OP));
+    break;
+  case 111:
+    st.push(Symbol(111));
+    st.push(Symbol(NonTerminal::Assignable_Expr_NT));
+    st.push(Symbol(NonTerminal::Prefix_NT));
+    break;
+  case 112:
+    st.push(Symbol(112));
+    st.push(Symbol(NonTerminal::Index_Expr_NT));
+    st.push(Symbol(TokenType::NOT_KW));
+    break;
+  case 113:
+    st.push(Symbol(113));
+    st.push(Symbol(NonTerminal::maybe_Postfix_NT));
+    st.push(Symbol(NonTerminal::Index_Expr_NT));
+    break;
+  case 114:
+    st.push(Symbol(114));
+    st.push(Symbol(TokenType::INCREMENT_OP));
+    break;
+  case 115:
+    st.push(Symbol(115));
+    st.push(Symbol(TokenType::DECREMENT_OP));
+    break;
+  case 116:
+    st.push(Symbol(116));
+    break;
+  case 117:
+    st.push(Symbol(117));
+    st.push(Symbol(TokenType::INCREMENT_OP));
+    break;
+  case 118:
+    st.push(Symbol(118));
+    st.push(Symbol(TokenType::DECREMENT_OP));
+    break;
+  case 119:
+    st.push(Symbol(119));
+    st.push(Symbol(NonTerminal::Index_Chain_NT));
+    st.push(Symbol(NonTerminal::CallOrVariable_NT));
+    break;
+  case 120:
+    st.push(Symbol(120));
+    st.push(Symbol(NonTerminal::Primary_Expr_NT));
+    break;
+  case 121:
+    st.push(Symbol(121));
+    break;
+  case 122:
+    st.push(Symbol(122));
+    st.push(Symbol(NonTerminal::Index_Chain_NT));
+    st.push(Symbol(TokenType::RIGHT_CURLY_PR));
+    st.push(Symbol(NonTerminal::Expr_NT));
+    st.push(Symbol(TokenType::LEFT_CURLY_PR));
+    break;
+  case 123:
+    st.push(Symbol(123));
+    st.push(Symbol(TokenType::RIGHT_PR));
+    st.push(Symbol(NonTerminal::Expr_NT));
+    st.push(Symbol(TokenType::LEFT_PR));
+    break;
+  case 124:
+    st.push(Symbol(124));
+    st.push(Symbol(NonTerminal::Literal_NT));
+    break;
+  case 125:
+    st.push(Symbol(125));
+    st.push(Symbol(TokenType::INTEGER_NUM));
+    break;
+  case 126:
+    st.push(Symbol(126));
+    st.push(Symbol(TokenType::FLOAT_NUM));
+    break;
+  case 127:
+    st.push(Symbol(127));
+    st.push(Symbol(TokenType::STRING_SY));
+    break;
+  case 128:
+    st.push(Symbol(128));
+    st.push(Symbol(TokenType::TRUE_KW));
+    break;
+  case 129:
+    st.push(Symbol(129));
+    st.push(Symbol(TokenType::FALSE_KW));
+    break;
+  case 130:
+    st.push(Symbol(130));
+    st.push(Symbol(NonTerminal::MaybeCall_NT));
+    st.push(Symbol(TokenType::ID_SY));
+    break;
+  case 131:
+    st.push(Symbol(131));
+    break;
+  case 132:
+    st.push(Symbol(132));
+    st.push(Symbol(NonTerminal::Call_Expr_NT));
+    break;
+  case 133:
+    st.push(Symbol(133));
+    st.push(Symbol(TokenType::RIGHT_PR));
+    st.push(Symbol(NonTerminal::Call_Expr_Tail_NT));
+    st.push(Symbol(NonTerminal::Expr_NT));
+    st.push(Symbol(TokenType::LEFT_PR));
+    break;
+  case 134:
+    st.push(Symbol(134));
+    break;
+  case 135:
+    st.push(Symbol(135));
+    st.push(Symbol(NonTerminal::Call_Expr_Tail_NT));
+    st.push(Symbol(NonTerminal::Expr_NT));
+    st.push(Symbol(TokenType::COMMA_SY));
+    break;
+  }
 }

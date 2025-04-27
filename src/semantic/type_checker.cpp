15f111e7b5c367426d8920fe8a4ad8cc394f3c6e
ITachi-Is-Better <y2ngboy@gmail.com>
2025-04-27 16:22:38 +0300

    adding a helper method `is_number` to `typechecker`class and optimize the `typechecker` class

--- src/semantic/type_checker.cpp
+++ src/semantic/type_checker.cpp
@@ -4,6 +4,11 @@ TypeChecker::TypeChecker() {}
 
 SymbolType TypeChecker::is_valid_assign(SymbolType left, SymbolType right, int dim_left, int dim_right)
 {
+  if (dim_left != dim_right)
+  {
+    return SymbolType::Undefined;
+  }
+
   if ((left == SymbolType::Boolean || right == SymbolType::Boolean) && left != right)
   {
     return SymbolType::Undefined;
@@ -12,16 +17,11 @@ SymbolType TypeChecker::is_valid_assign(SymbolType left, SymbolType right, int d
   {
     return SymbolType::Undefined;
   }
-  else if (!((left == SymbolType::Integer || left == SymbolType::Float) &&
-             (right == SymbolType::Integer || right == SymbolType::Float)))
+  if (right == SymbolType::Float)
   {
-    return SymbolType::Undefined;
+    return right;
   }
 
-  if (dim_left != dim_right)
-  {
-    return SymbolType::Undefined;
-  }
   return left;
 }
 
@@ -45,24 +45,18 @@ SymbolType TypeChecker::is_valid_equality(SymbolType left, SymbolType right)
   {
     return SymbolType::Undefined;
   }
-  else if (!((left == SymbolType::Integer || left == SymbolType::Float) &&
-             (right == SymbolType::Integer || right == SymbolType::Float)))
-  {
-    return SymbolType::Undefined;
-  }
 
   return SymbolType::Boolean;
 }
 
 SymbolType TypeChecker::is_valid_relational(SymbolType left, SymbolType right)
 {
-  if (!((left == SymbolType::Integer || left == SymbolType::Float) &&
-        (right == SymbolType::Integer || right == SymbolType::Float)))
+  if (is_number(left) && is_number(right))
   {
-    return SymbolType::Undefined;
+    return SymbolType::Boolean;
   }
 
-  return SymbolType::Boolean;
+  return SymbolType::Undefined;
 }
 
 SymbolType TypeChecker::is_valid_addition(SymbolType left, SymbolType right, string op)
@@ -76,16 +70,14 @@ SymbolType TypeChecker::is_valid_addition(SymbolType left, SymbolType right, str
         return SymbolType::Undefined;
       }
     }
-    else if (!(left == SymbolType::Integer || left == SymbolType::Float) ||
-             !(right == SymbolType::Integer || right == SymbolType::Float))
+    else if (!is_number(left) || !is_number(right))
     {
       return SymbolType::Undefined;
     }
   }
   else
   {
-    if (!(left == SymbolType::Integer || left == SymbolType::Float) ||
-        !(right == SymbolType::Integer || right == SymbolType::Float))
+    if (!is_number(left) || !is_number(right))
     {
       return SymbolType::Undefined;
     }
@@ -114,8 +106,7 @@ SymbolType TypeChecker::is_valid_multiplicative(SymbolType left, SymbolType righ
   }
   else
   {
-    if (!((left == SymbolType::Integer || left == SymbolType::Float) &&
-          (right == SymbolType::Integer || right == SymbolType::Float)))
+    if (!is_number(left) || !is_number(right))
     {
       return SymbolType::Undefined;
     }
@@ -133,7 +124,7 @@ SymbolType TypeChecker::is_valid_Unary(SymbolType operand, string op, int dim)
 {
   if (op == "-")
   {
-    if (operand != SymbolType::Integer && operand != SymbolType::Float)
+    if (!is_number(operand))
     {
       return SymbolType::Undefined;
     }
@@ -150,10 +141,11 @@ SymbolType TypeChecker::is_valid_Unary(SymbolType operand, string op, int dim)
   }
   else if (op == "++" || op == "--")
   {
-    if (operand != SymbolType::Integer && operand != SymbolType::Float)
+    if (!is_number(operand))
     {
       return SymbolType::Undefined;
     }
+
     return operand;
   }
   else if (op == "$")
@@ -166,7 +158,7 @@ SymbolType TypeChecker::is_valid_Unary(SymbolType operand, string op, int dim)
   }
   else if (op == "@")
   {
-    if (operand != SymbolType::Integer && operand != SymbolType::Float && operand != SymbolType::Boolean)
+    if (!is_number(operand) && operand != SymbolType::Boolean)
     {
       return SymbolType::Undefined;
     }
@@ -184,4 +176,9 @@ SymbolType TypeChecker::is_valid_Unary(SymbolType operand, string op, int dim)
     return SymbolType::Integer;
   }
   return SymbolType::Undefined;
+}
+
+bool TypeChecker::is_number(SymbolType t)
+{
+  return t == SymbolType::Integer || t == SymbolType::Float;
 }

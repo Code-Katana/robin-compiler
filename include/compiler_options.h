7d01fbbc8746636e67b0cb5a7736af6633e6cc41
ITachi-Is-Better <y2ngboy@gmail.com>
2025-05-05 15:33:01 +0300

    adding 2 constructors with the optimization

--- include/compiler_options.h
+++ include/compiler_options.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <string>
-
+#include "code_optimization.h"
 using namespace std;
 
 enum class ScannerOptions
@@ -21,16 +21,20 @@ class CompilerOptions
 {
 public:
   CompilerOptions(const string &prog);
+  CompilerOptions(const string &prog, const CodeOptimization &opt);
   CompilerOptions(const string &prog, const ParserOptions &pr);
   CompilerOptions(const string &prog, const ScannerOptions &sc);
   CompilerOptions(const string &prog, const ScannerOptions &sc, const ParserOptions &pr);
+  CompilerOptions(const string &prog, const ScannerOptions &sc, const ParserOptions &pr, const CodeOptimization &opt);
 
   string get_program();
   ScannerOptions get_scanner_option();
   ParserOptions get_parser_option();
+  CodeOptimization get_optimization_option();
 
 private:
   string program;
   ScannerOptions scOption;
   ParserOptions prOption;
+  CodeOptimization optOption;
 };
--- src/compiler/compiler_options.cpp
+++ src/compiler/compiler_options.cpp
@@ -1,13 +1,41 @@
 #include "compiler_options.h"
 
-CompilerOptions::CompilerOptions(const string &prog) : program(prog), scOption(ScannerOptions::FiniteAutomaton), prOption(ParserOptions::RecursiveDecent) {}
+CompilerOptions::CompilerOptions(const string &prog)
+    : program(prog),
+      scOption(ScannerOptions::FiniteAutomaton),
+      prOption(ParserOptions::RecursiveDecent),
+      optOption(CodeOptimization()) {}
 
-CompilerOptions::CompilerOptions(const string &prog, const ParserOptions &pr) : program(prog), scOption(ScannerOptions::FiniteAutomaton), prOption(pr) {}
+CompilerOptions::CompilerOptions(const string &prog, const CodeOptimization &opt)
+    : program(prog),
+      scOption(ScannerOptions::FiniteAutomaton),
+      prOption(ParserOptions::RecursiveDecent),
+      optOption(opt) {}
 
-CompilerOptions::CompilerOptions(const string &prog, const ScannerOptions &sc) : program(prog), scOption(sc), prOption(ParserOptions::RecursiveDecent) {}
+CompilerOptions::CompilerOptions(const string &prog, const ParserOptions &pr)
+    : program(prog),
+      scOption(ScannerOptions::FiniteAutomaton),
+      prOption(pr),
+      optOption(CodeOptimization()) {}
 
-CompilerOptions::CompilerOptions(const string &prog, const ScannerOptions &sc, const ParserOptions &pr) : program(prog), scOption(sc), prOption(pr) {}
+CompilerOptions::CompilerOptions(const string &prog, const ScannerOptions &sc)
+    : program(prog),
+      scOption(sc),
+      prOption(ParserOptions::RecursiveDecent),
+      optOption(CodeOptimization()) {}
 
+CompilerOptions::CompilerOptions(const string &prog, const ScannerOptions &sc, const ParserOptions &pr)
+    : program(prog),
+      scOption(sc),
+      prOption(pr),
+      optOption(CodeOptimization()) {}
+
+// *** MISSING CONSTRUCTOR: ***
+CompilerOptions::CompilerOptions(const string &prog, const ScannerOptions &sc, const ParserOptions &pr, const CodeOptimization &opt)
+    : program(prog),
+      scOption(sc),
+      prOption(pr),
+      optOption(opt) {}
 string CompilerOptions::get_program()
 {
   return program;
@@ -21,4 +49,8 @@ ScannerOptions CompilerOptions::get_scanner_option()
 ParserOptions CompilerOptions::get_parser_option()
 {
   return prOption;
+}
+CodeOptimization CompilerOptions::get_optimization_option()
+{
+  return optOption;
 }

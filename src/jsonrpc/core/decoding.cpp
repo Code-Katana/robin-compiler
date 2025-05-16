dd7b62af294c546b7cd1b470c76a09b0a39202ac
samirahmad5445 <samirahmad5445@gmail.com>
2025-05-16 04:54:53 +0300

    feat: json encoding and decoding

---
+++ src/jsonrpc/core/decoding.cpp
@@ -0,0 +1,292 @@
+#include "jsonrpc/core/decoding.h"
+
+namespace json
+{
+  // Decoder Class
+  class Decoder
+  {
+  public:
+    static Json *parse(const string &str);
+
+  private:
+    Decoder();
+
+    // parse functions
+    static Value *parse_value();
+    static Json *parse_json();
+    static Object *parse_object();
+    static Array *parse_array();
+    static String *parse_string();
+    static Number *parse_number();
+    static Value *parse_keyword();
+
+    // helper functions
+    static char peek();
+    static char consume();
+    static string slice(int s, int e);
+    static bool is_keyword(const string &str);
+    static int get_length();
+    static void skip_whitespace();
+
+    static string *source;
+    static int current_index;
+  };
+
+  // members
+  int Decoder::current_index = 0;
+  string *Decoder::source = new string();
+
+  // helpers
+  char Decoder::peek()
+  {
+    return (*source)[current_index];
+  }
+
+  char Decoder::consume()
+  {
+    return (*source)[current_index++];
+  }
+
+  string Decoder::slice(int s, int e)
+  {
+    return (*source).substr(s, e);
+  }
+
+  bool Decoder::is_keyword(const string &str)
+  {
+    return str == keywords::TRUE_KEYWORD || str == keywords::FALSE_KEYWORD || str == keywords::NULL_KEYWORD;
+  }
+
+  int Decoder::get_length()
+  {
+    return (*source).length();
+  }
+
+  void Decoder::skip_whitespace()
+  {
+    while (isspace(peek()) && current_index < get_length())
+    {
+      consume();
+    }
+  }
+
+  // parsing
+  Json *Decoder::parse(const string &str)
+  {
+    *source = str;
+    current_index = 0;
+
+    return parse_json();
+  }
+
+  Value *Decoder::parse_value()
+  {
+    skip_whitespace();
+
+    if (peek() == '{')
+    {
+      return parse_object();
+    }
+    else if (peek() == '[')
+    {
+      return parse_array();
+    }
+    else if (peek() == '\"' || peek() == '\'')
+    {
+      return parse_string();
+    }
+    else if (isdigit(peek()) || peek() == '-' || peek() == '+')
+    {
+      return parse_number();
+    }
+    else if (is_keyword(slice(current_index, current_index + 4)))
+    {
+      return parse_keyword();
+    }
+
+    return nullptr;
+  }
+
+  Json *Decoder::parse_json()
+  {
+    if (peek() != '{' && peek() != '[')
+    {
+      return nullptr;
+    }
+
+    return (Json *)parse_value();
+  }
+
+  Object *Decoder::parse_object()
+  {
+    if (peek() != '{')
+    {
+      return nullptr;
+    }
+
+    consume();
+    skip_whitespace();
+
+    Object *object = new Object();
+
+    while (peek() != '}')
+    {
+      if (peek() == ',')
+      {
+        consume();
+        skip_whitespace();
+      }
+
+      string key = parse_string()->value;
+      skip_whitespace();
+
+      if (peek() != ':')
+      {
+        return nullptr;
+      }
+
+      consume();
+      skip_whitespace();
+
+      Value *value = parse_value();
+
+      if (!value)
+      {
+        return nullptr;
+      }
+
+      object->add(key, value);
+      skip_whitespace();
+    }
+
+    if (peek() != '}')
+    {
+      return nullptr;
+    }
+
+    consume();
+    return object;
+  }
+
+  Array *Decoder::parse_array()
+  {
+    if (peek() != '[')
+    {
+      return nullptr;
+    }
+
+    consume();
+    skip_whitespace();
+
+    Array *array = new Array();
+
+    while (peek() != ']')
+    {
+      Value *item = parse_value();
+
+      if (!item)
+      {
+        return nullptr;
+      }
+
+      array->add(item);
+      skip_whitespace();
+
+      if (peek() == ',')
+      {
+        consume();
+      }
+
+      skip_whitespace();
+    }
+
+    if (peek() != ']')
+    {
+      return nullptr;
+    }
+
+    consume();
+    return array;
+  }
+
+  String *Decoder::parse_string()
+  {
+    string str = "";
+    bool is_single_quote = false;
+
+    if (peek() == '\'')
+    {
+      is_single_quote = true;
+      consume();
+    }
+    else if (peek() == '\"')
+    {
+      consume();
+    }
+
+    while (peek() != (is_single_quote ? '\'' : '\"') && current_index < get_length())
+    {
+      str += consume();
+    }
+
+    if (peek() != (is_single_quote ? '\'' : '\"'))
+    {
+      return nullptr;
+    }
+
+    consume();
+    return new String(str);
+  }
+
+  Number *Decoder::parse_number()
+  {
+    string num = "";
+    bool is_decimal = false;
+
+    while (isdigit(peek()) || peek() == '.')
+    {
+      if (peek() == '.')
+      {
+        if (is_decimal)
+        {
+          return nullptr;
+        }
+
+        is_decimal = true;
+      }
+
+      num += consume();
+    }
+
+    return new Number(num);
+  }
+
+  Value *Decoder::parse_keyword()
+  {
+    string str = slice(current_index, current_index + 4);
+    current_index += 4;
+
+    if (str == keywords::TRUE_KEYWORD || str == keywords::FALSE_KEYWORD)
+    {
+      return new Boolean(str);
+    }
+    else if (str == keywords::NULL_KEYWORD)
+    {
+      return new Null();
+    }
+
+    return nullptr;
+  }
+
+  Value *decode(const string &str)
+  {
+    return Decoder::parse(str);
+  }
+
+  Value *decode_from_file(const string &path)
+  {
+    ifstream file(path);
+    string str((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
+    return Decoder::parse(str);
+  }
+}
---
+++ src/jsonrpc/core/encoding.cpp
@@ -0,0 +1,109 @@
+#include "jsonrpc/core/encoding.h"
+
+namespace json
+{
+  class Encoder
+  {
+  public:
+    static string stringify(Json *val);
+
+  private:
+    Encoder();
+
+    // decoding functions
+    static string stringify_value(Value *arr);
+    static string stringify_object(Object *obj);
+    static string stringify_array(Array *arr);
+
+    // helper functions
+    static string quote(string s);
+  };
+
+  // decoding functions
+  string Encoder::stringify(Json *val)
+  {
+    return stringify_value(val);
+  }
+
+  string Encoder::stringify_value(Value *value)
+  {
+    switch (value->type)
+    {
+    case ValueType::OBJECT:
+      return stringify_object(static_cast<Object *>(value));
+
+    case ValueType::ARRAY:
+      return stringify_array(static_cast<Array *>(value));
+
+    case ValueType::STRING:
+      return quote(static_cast<String *>(value)->value);
+
+    case ValueType::BOOLEAN:
+      return static_cast<Boolean *>(value)->value ? keywords::TRUE_KEYWORD : keywords::FALSE_KEYWORD;
+
+    case ValueType::NUMBER:
+      return to_string(static_cast<Number *>(value)->value);
+
+    case ValueType::NULL_VALUE:
+      return keywords::NULL_KEYWORD;
+    default:
+      throw invalid_argument("Invalid value type.");
+    }
+  }
+
+  string Encoder::stringify_object(Object *obj)
+  {
+    string stringified_json = "{";
+    vector<string> keys = obj->keys();
+
+    for (auto key : keys)
+    {
+      stringified_json += quote(key) + ": " + stringify_value(obj->props.at(key));
+
+      if (key != keys.back())
+      {
+        stringified_json += ',';
+      }
+    }
+
+    stringified_json += "}";
+    return stringified_json;
+  }
+
+  string Encoder::stringify_array(Array *arr)
+  {
+    string stringified_json = "[";
+
+    for (auto value : arr->values)
+    {
+      stringified_json += stringify_value(value);
+
+      if (value != arr->values.back())
+      {
+        stringified_json += ", ";
+      }
+    }
+
+    stringified_json += ']';
+    return stringified_json;
+  }
+
+  // helper functions
+  string Encoder::quote(string s)
+  {
+    return "\"" + s + "\"";
+  }
+
+  // the public interface
+  string encode(Json *value)
+  {
+    return Encoder::stringify(value);
+  }
+
+  void encode_to_file(Json *value, string path)
+  {
+    ofstream file(path);
+    file << encode(value);
+    file.close();
+  }
+}
